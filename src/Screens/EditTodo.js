4851f80fdfe47e9b1c3f4b3a1cfbfaf15218c3dc
navid798 <56483402+navid798@users.noreply.github.com>
2020-06-16 17:56:18 +0430

    clean some code

--- src/Screens/EditTodo.js
+++ src/Screens/EditTodo.js
@@ -3,23 +3,21 @@ import { View, Text, StyleSheet } from "react-native";
 
 const EditTodo = () => {
   return (
-<<<<<<< HEAD
+
     <View>
       <Text>Edit Todo</Text>
-=======
+
     <View >
       <Text>Edit</Text>
->>>>>>> df0f07cef44e6480a34e218ed7a35b8e59580725
+
     </View>
   );
 };
 
-<<<<<<< HEAD
 const styles = StyleSheet.create({});
-=======
+
 const styles = StyleSheet.create({
   
 });
->>>>>>> df0f07cef44e6480a34e218ed7a35b8e59580725
 
 export default EditTodo;
