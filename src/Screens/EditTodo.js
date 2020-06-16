549816e5f976973a6c39ed6fae06e62d48b2a005
navid798 <56483402+navid798@users.noreply.github.com>
2020-06-16 16:38:51 +0430

    Update EditTodo.js

--- src/Screens/EditTodo.js
+++ src/Screens/EditTodo.js
@@ -1,25 +1,16 @@
-//import liraries
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 
-// create a component
-const MyComponent = () => {
+const EditTodo = () => {
   return (
-    <View style={styles.container}>
-      <Text>MyComponent</Text>
+    <View >
+      <Text>Edit</Text>
     </View>
   );
 };
 
-// define your styles
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#2c3e50",
-  },
+  
 });
 
-//make this component available to the app
-export default MyComponent;
+export default EditTodo;
