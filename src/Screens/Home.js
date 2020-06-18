17b1a46d0bd74ba7a57b027f64c194fbec829096
navid798 <56483402+navid798@users.noreply.github.com>
2020-06-18 15:15:59 +0430

    state rename to 'todos'

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -10,7 +10,7 @@ import Icon from "react-native-ionicons";
 import Context from "../../Context";
 
 const Home = ({ navigation }) => {
-  const { tods, deletetodo } = useContext(Context);
+  const { todos, deletetodo } = useContext(Context);
   return (
     <View>
       <FlatList
